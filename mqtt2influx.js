3f246fb39ab384584eed0b681023b5565b3f3fa8
Benjamin Jung <bjung@team-jung.de>
2018-02-26 21:52:28 +0100

    Made MQTT more robust.

--- mqtt2influx.js
+++ mqtt2influx.js
@@ -1,13 +1,24 @@
 var mqtt = require('mqtt')
-var client = mqtt.connect('mqtt://test.mosquitto.org')
 
-client.on('connect', function () {
-    client.subscribe('presence')
-    client.publish('presence', 'Hello mqtt')
+const MQTT_ADDR = 'mqtt://rasperrypi:18831'
+const MQTT_CLIENTID = 'mqtt2influx'
+
+
+var client = mqtt.connect(MQTT_ADDR, {
+    clientId: MQTT_CLIENTID,
+    connectTimeout: 1000,
+    reconnecting: true,
+    debug: true
 })
 
+client.on('connect', () => client.subscribe('#', (err) => console.log(err)))
+
+client.on('offline', () => console.log('Offline!!'))
+
+client.on('reconnect', () => console.log(`Trying reconnect to ${MQTT_ADDR}`))
+
+client.on('error', (error) => console.log(error))
+
 client.on('message', function (topic, message) {
-    // message is Buffer
-    console.log(message.toString())
-    client.end()
+    console.log(`Topic: ${topic} Message: ${message.toString()}`)
 })
