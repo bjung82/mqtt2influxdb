f73ceb6092428a30f7716aabdafbd091f06f996e
Benjamin Jung <bjung@team-jung.de>
2018-02-27 22:11:39 +0100

    First working implementation with push to influx.

--- mqtt2influx.js
+++ mqtt2influx.js
@@ -1,24 +1,42 @@
 var mqtt = require('mqtt')
+var Influx = require('influxdb-nodejs')
 
-const MQTT_ADDR = 'mqtt://rasperrypi:18831'
+const MQTT_ADDR = 'mqtt://rasperrypi:1883'
 const MQTT_CLIENTID = 'mqtt2influx'
 
+const INFLUXDB_DBNAME = 'sensor_data'
 
-var client = mqtt.connect(MQTT_ADDR, {
+const mqttClient = mqtt.connect(MQTT_ADDR, {
     clientId: MQTT_CLIENTID,
     connectTimeout: 1000,
     reconnecting: true,
     debug: true
 })
 
-client.on('connect', () => client.subscribe('#', (err) => console.log(err)))
+const influxClient = new Influx(`http://rasperrypi:8086/${INFLUXDB_DBNAME}`)
+const fieldSchema = {
+    'esp32/temperature' : 'f',
+    'esp32/humidity' : 'f',
+    'esp32/lux' : 'f',
+    'esp32/wifi-rssi' : 'f'
+}
+const tagSchema = {}
+influxClient.schema('data', fieldSchema, tagSchema, {stripInknown : true })
 
-client.on('offline', () => console.log('Offline!!'))
+mqttClient.on('connect', () => mqttClient.subscribe('#', (err) => console.log(err)))
 
-client.on('reconnect', () => console.log(`Trying reconnect to ${MQTT_ADDR}`))
+mqttClient.on('offline', () => console.log('Offline!!'))
 
-client.on('error', (error) => console.log(error))
+mqttClient.on('reconnect', () => console.log(`Trying reconnect to ${MQTT_ADDR}`))
 
-client.on('message', function (topic, message) {
+mqttClient.on('error', (error) => console.log(error))
+
+mqttClient.on('message', function (topic, message) {
     console.log(`Topic: ${topic} Message: ${message.toString()}`)
+
+    let data = {}
+    data[topic] = String(message)
+
+    influxClient.write('data')
+        .field(data).then(() => {}).catch(e => console.error(e))
 })
